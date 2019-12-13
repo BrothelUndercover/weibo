a205ba9af4c6c344419ab7bc32c96a53c7ab491c
thiago ta <thiagota@thiagodeMacBook-Pro.local>
2019-12-13 19:27:31 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+//注册页面
 Route::get('sigup','UsersController@create')->name('signup');
+
+Route::resource('users','UsersController');
