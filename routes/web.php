e30aa69c423b009a0cf045ad54d6a02bac22599c
thiago ta <thiagota@thiagodeMacBook-Pro.local>
2019-12-14 19:34:24 +0800

    登录创建会话

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,9 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //注册页面
 Route::get('signup','UsersController@create')->name('signup');
 
+//登录注册
+Route::get('login','SessionsCotroller@create')->name('login');
+Route::post('login','SessionsCotroller@store')->name('login');
+Route::delete('logout','SessionsCotroller@destroy')->name('logout');
+
 Route::resource('users','UsersController');
