8935ed6bf2e33c90f92019dc68cd2554986bcdf8
vagrant <vagrant@homestead>
2019-12-30 09:59:18 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,17 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderBy('created_at','desc');
     }
+
+    //获取粉丝 (被关注)
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    //获取关注人（主动关注）
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
 }
